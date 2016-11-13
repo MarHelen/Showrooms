6f731f87f047487be44f458e4e9cf2e31c534ec0
System Administrator <root@Olenas-MacBook-Pro.local>
2016-11-13 22:04:47 +0200

    gallery changes

---
+++ static/js/details.js
@@ -0,0 +1,103 @@
+    //Filename: details.js
+
+  //var showroom = 
+ 
+ /* window.onload=function(place){
+    placeMarker(place);
+};*/
+
+  var fb_url = '/' + showroom.placeId + '/';
+
+   window.onload=function(){
+    
+    placeMarker(showroom);
+    };
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '314614308897367',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+    $(document).trigger('fbload'); 
+  };
+  
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+//var fb_url = '/{{ place.placeId }}/';
+
+$(document).on(
+    'fbload',  
+    function(){
+FB.api(
+  fb_url,
+  'GET',
+  {"fields":"posts.limit(20){instagram_eligibility,full_picture,message,link}", 
+   "access_token" : "314614308897367|pd6XzgFTKRFPb7kP_Zzu9v_0wbk" },
+  function(response) {
+    var posts_object = response["posts"]; 
+    
+    $.each( posts_object["data"], function(i,post){
+      if (i === 0){
+        var first = $("#carusel_first");
+        first.find('img').attr("src", post["full_picture"]);
+      }
+      else {
+        var Block = '<div class="item"><img src='+ post["full_picture"] +
+                    ' alt="" class="img-responsive img-fluid center-block" style="hight:100%; background-position: center center"></div>';
+        //'<div class="carousel-caption" ><p class="lead well"><small>'+ post["message"] +'</small></p></div></div>';
+        var carusel = $("#carusel-body");
+        $("#carusel-body div:last").after(Block);
+      }
+    });
+  }
+);
+});
+
+$(document).on(
+    'fbload',  
+    function(){
+FB.api(
+  fb_url,
+  'GET',
+  {"fields":"posts.limit(21){instagram_eligibility,full_picture,message,link}", 
+   "access_token" : "314614308897367|pd6XzgFTKRFPb7kP_Zzu9v_0wbk" },
+  function(response) {
+    var posts_object = response["posts"]; 
+    
+    $.each( posts_object["data"], function(i,post){
+
+        var Block = '<div class="clearfix col-sm-5 col-md-4 " id="image"><a href='+ post["link"] +' target="_blank"><img src='+ post["full_picture"] +
+                    ' class="img-responsive img-fluid center-block" style="hight:100%; width:100%; background-position: center center"></a>';
+        if (post["message"]){
+          var message;
+          var message_len_limit = 100;
+          if (post["message"].length > message_len_limit){
+            Block += '<br class="">' + post["message"].slice(0,message_len_limit-1) +
+            '<a href="#" data-toggle="tooltip" data-html="true" data-placement="bottom" title='+ post["message"] + '>...</a></div></div>';
+          }
+          else 
+            Block += '<br class="">'+ post["message"] +'</div></div>';
+
+        }
+        else Block += '<br class=""></div></div>';
+        var gallery = $("#gallery-body");
+        if (i === 0){
+          $("#gallery-body").prepend(Block);
+        }
+        else  {
+          $("#gallery-body #image:last").after(Block);
+        }
+      
+    });
+  }
+);
+});
+
