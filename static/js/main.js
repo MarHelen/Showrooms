770b93e630b9aa67b100034c00d10b5a433efe5b
System Administrator <root@Olenas-MacBook-Pro.local>
2016-12-06 00:28:35 +0200

    bootstrap4 list js fix, adding font-awesome

--- static/js/main.js
+++ static/js/main.js
@@ -4,8 +4,8 @@
 
   function build_list(place, i){
     //map_list content building
-    var Content = '<tr><td><div class = "card" id="'+ i + '"><div class = "card-header clickable panel-collapsed" id="panel-header">'+
-    '<h6 class = "card-header">'+place.title +'</h6><span class="pull-right clickable panel-collapsed" ><i class="glyphicon glyphicon-chevron-down"></i></span></div>'+
+    var Content = '<tr><td><div class = "card list-card" id="'+ i + '"><div class = "card-header clickable panel-collapsed" id="panel-header">'+
+    '<h6 class = "card-header">'+place.title +'<i class="pull-right clickable panel-collapsed fa-chevron-down fa"></i></h6></div>'+
     '<div class="card-block" style="display: none" >';
     Content += build_media(place) + '</div></div></td></tr>';
     
@@ -23,7 +23,7 @@
     $this.parents('.card').find('.card-body').slideUp();
     $this.addClass('panel-collapsed');
     $this.parents('.card').find('.card-block').css('display', 'none');
-    $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+    $this.find('i').removeClass('fa-chevron-up').addClass('fa-chevron-down');
     //getting a number of appropriate marker
     var panel_id = $this.parents('.card').attr('id');
     marker_animation(panel_id);
@@ -32,7 +32,7 @@
     $this.parents('.card').find('.card-body').slideDown();
     $this.removeClass('panel-collapsed');
     $this.parents('.card').find('.card-block').css('display', 'block');
-    $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+    $this.find('i').removeClass('fa-chevron-down').addClass('fa-chevron-up');
 
     //getting a number of appropriate marker
     var panel_id = $this.parents('.card').attr('id');
@@ -51,40 +51,50 @@
       var t_body = temp.parents('.table').find('tbody .content');
       if (temp.hasClass('collapsed') ){
         //resize list div
-        temp.removeClass('col-md-1');
-        temp.removeClass('col-sm-1');
         temp.addClass('col-md-5');
         temp.addClass('col-sm-5');
+        temp.addClass('col-lg-5');
+        temp.removeClass('col-md-1');
+        temp.removeClass('col-sm-1');
+        temp.removeClass('col-lg-1');
         
         temp.removeClass('hidden');
+        temp.css('display','block');
         temp.removeClass('collapsed');
         //resize map div
         temp2.removeClass('col-md-9');
         temp2.removeClass('col-sm-9');
+        temp2.removeClass('col-lg-9');
         temp2.addClass('col-md-5');
         temp2.addClass('col-sm-5');
+        temp2.addClass('col-lg-5');
         //resize and recenter google map object
         google.maps.event.trigger(map, "resize");
-        map.setCenter(Kiev_center);
+        map.setCenter(center);
         //var div_map = temp2.parents('div').find('.google-map-canvas');
         
       } else {
         //resize list div
         temp.removeClass('col-md-5');
         temp.removeClass('col-sm-5');
+        temp.removeClass('col-lg-5');
         temp.addClass('col-md-1');
         temp.addClass('col-sm-1');
+        temp.addClass('col-lg-1');
         
         temp.addClass('hidden');
+        temp.css('display', 'none');
         temp.addClass('collapsed');
         //resize map div
         temp2.removeClass('col-md-5');
         temp2.removeClass('col-sm-5');
+        temp2.removeClass('col-lg-5');
         temp2.addClass('col-md-9');
         temp2.addClass('col-sm-9');
+        temp2.addClass('col-lg-9');
         //resize and recenter google map object
         google.maps.event.trigger(map, "resize");
-        map.setCenter(Kiev_center);
+        map.setCenter(center);
         //var div_map = temp2.parents('div').find('.google-map-canvas');
         
       }
